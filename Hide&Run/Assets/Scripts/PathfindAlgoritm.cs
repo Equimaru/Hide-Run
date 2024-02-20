121e1c8413de14c41e51238b4ee97491d238e2b4
Equimaru <119777912+Equimaru@users.noreply.github.com>
2024-02-20 05:02:42 +0300

    Assets for jump (Run/Idle) animations added

---
+++ Hide&Run/Assets/Scripts/PathfindAlgoritm.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathfindAlgoritm : MonoBehaviour
+{
+    [SerializeField] LayerMask unwalkableMask;
+
+    [SerializeField] Vector2 gridWorldSize;
+
+    [SerializeField] private float nodeRadius = 1f;
+    private float nodeDiameter;
+
+     
+
+}
