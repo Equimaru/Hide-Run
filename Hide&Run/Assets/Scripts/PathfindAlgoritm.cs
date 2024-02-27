5f93b25fced7c1878b59254442b54a55cdb6c91e
Equimaru <119777912+Equimaru@users.noreply.github.com>
2024-02-27 19:35:17 +0300

    GetObjectNode (Player application)

--- Hide&Run/Assets/Scripts/PathfindAlgoritm.cs
+++ Hide&Run/Assets/Scripts/PathfindAlgoritm.cs
@@ -9,6 +9,8 @@ public class PathfindAlgoritm : MonoBehaviour
 
     [SerializeField] private Vector2 gridWorldSize;
 
+    [SerializeField] private Transform playerPosition;
+
     [SerializeField] private float nodeRadius;
     private float nodeDiameter;
 
@@ -50,12 +52,29 @@ public class PathfindAlgoritm : MonoBehaviour
         }
     }
 
+    private Node GetObjectNode(Vector3 objectPosition)
+    {
+        float positionInPercentX = (objectPosition.x + gridWorldSize.x/2) / gridWorldSize.x;
+        float positionInPercentY = (objectPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;
+
+        positionInPercentX = Mathf.Clamp(positionInPercentX, 0, 1);
+        positionInPercentY = Mathf.Clamp(positionInPercentY, 0, 1);
+
+        int x = Mathf.RoundToInt((gridWorldSize.x - 1) * positionInPercentX);
+        int y = Mathf.RoundToInt((gridWorldSize.y - 1) * positionInPercentY);
+
+
+        return grid[x, y];
+    }
+
     private void OnDrawGizmos()
     {
         if (grid == null) return;
+        Node playerNode = GetObjectNode(playerPosition.transform.position);
         foreach (Node n in grid)
         {
             Gizmos.color = (n.unwalkable) ? Color.red : Color.white;
+            if (n == playerNode) Gizmos.color = Color.green;
             Gizmos.DrawWireCube(n.nodeCenter, new Vector3(nodeDiameter - 0.1f, nodeDiameter - 0.1f, nodeDiameter - 0.1f));
         }
     }
